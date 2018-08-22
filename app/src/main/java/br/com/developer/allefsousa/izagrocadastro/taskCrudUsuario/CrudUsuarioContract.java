6298211ab57a0a410cc4a987f7f7b804640268ba
Allef sousa <allefsousa_1@hotmail.com>
2018-08-22 02:20:38 -0300

    Preparando app para Arquitetura MVP

---
+++ app/src/main/java/br/com/developer/allefsousa/izagrocadastro/taskCrudUsuario/CrudUsuarioContract.java
@@ -0,0 +1,16 @@
+package br.com.developer.allefsousa.izagrocadastro.taskCrudUsuario;
+
+/**
+ * Created by allef on 22/08/2018.
+ */
+
+public interface CrudUsuarioContract {
+
+    interface view{
+
+    }
+
+    interface presenter{
+
+    }
+}
