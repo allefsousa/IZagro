94aaa4a5be270345adacd37224c882fb5d551e82
Allef sousa <allefsousa_1@hotmail.com>
2018-08-22 10:54:01 -0300

    Configurações referentes a tabela

--- app/src/main/java/br/com/developer/allefsousa/izagrocadastro/data/Usuario.java
+++ app/src/main/java/br/com/developer/allefsousa/izagrocadastro/data/Usuario.java
@@ -5,6 +5,13 @@ package br.com.developer.allefsousa.izagrocadastro.data;
  */
 
 public class Usuario {
+    public static final String TABLE_NAME = "notes";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NOME = "nome";
+    public static final String COLUMN_SOBRENOME = "sobrenome";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_DATANASC = "datanasc";
+
     private int id;
     private String nome;
     private String sobrenome;
@@ -21,6 +28,17 @@ public class Usuario {
         this.email = email;
         this.dataNasc = dataNasc;
     }
+
+    // Create table SQL query
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + COLUMN_NOME + " TEXT,"
+                    + COLUMN_SOBRENOME + " TEXT,"
+                    + COLUMN_EMAIL + " TEXT,"
+                    + COLUMN_DATANASC + " TEXT"
+                    + ")";
+
 
     public int getId() {
         return id;
