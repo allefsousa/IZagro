3124effebeb374abb1c2fe088844a5c530d69ece
Allef sousa <allefsousa_1@hotmail.com>
2018-08-22 01:48:27 -0300

    Estrutura do Projeto

--- app/src/main/java/br/com/developer/allefsousa/izagrocadastro/taskCrudUsuario/MainActivity.java
+++ app/src/main/java/br/com/developer/allefsousa/izagrocadastro/taskCrudUsuario/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.developer.allefsousa.izagrocadastro;
+package br.com.developer.allefsousa.izagrocadastro.taskCrudUsuario;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.developer.allefsousa.izagrocadastro.R;
 
 public class MainActivity extends AppCompatActivity {
 
